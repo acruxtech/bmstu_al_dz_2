3d5caca9d4b51bfc88a802d546ce54025bee6513
acruxtech <acrux_tech@mail.ru>
2024-12-08 04:14:20 +0300

    feat: func support in expression

--- include/Calculator.hpp
+++ include/Calculator.hpp
@@ -5,9 +5,17 @@
 
 class Calculator {
 public:
-    double calc(const std::string expression);
+    double calc(const std::string& expression);
 
 private:
+    std::map<std::string, std::string> aliases = {
+        {"sin", "s"},
+        {"cos", "c"},
+        {"tag", "t"},
+        {"ctg", "g"},
+        {"exp", "e"}
+    };
+
     std::map<char, int> _operator_priorities {
         {'(', 0},
         {'+', 1},
@@ -16,11 +24,32 @@ private:
         {'/', 2},
         {'^', 3},
         {'~', 4},               // унарный минус
+        {'s', 4},               // sin
+        {'c', 4},               // cos
+        {'t', 4},               // tan
+        {'g', 4},              // ctg
+        {'e', 4},              // exp
     };
 
-    double _execute(char op, double a, double b);
+    double _execute(char op, double a, double b = 0) {
+        switch (op) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+            case '/': return a / b;
+            case '^': return pow(a, b);
+            case 's': return sin(a);
+            case 'c': return cos(a);
+            case 't': return tan(a);
+            case 'g': return 1 / tan(a);
+            case 'e': return exp(a);
+            default: return 0;
+        }
+    }
 
-    std::string _get_number_from_str(std::string seq, int& i);
+    std::string _get_number_from_str(const std::string& seq, int& i);
 
-    std::string _convert_to_postfix_notation(const std::string seq);
+    std::string _replace_with_aliases(const std::string& seq);
+
+    std::string _convert_to_postfix_notation(const std::string& source_seq);
 };
--- source/Calculator.cpp
+++ source/Calculator.cpp
@@ -1,26 +1,14 @@
 #include <cmath>
 #include <stack>
+#include <stdexcept>
 #include <string>
 #include "../include/Calculator.hpp"
 
 
-
-double Calculator::_execute(char op, double a, double b) {
-    switch (op) {
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
-        case '/': return a / b;
-        case '^': return pow(a, b);
-        default: return 0;
-    }
-}
-
-
-std::string Calculator::_get_number_from_str(std::string seq, int& i) {
+std::string Calculator::_get_number_from_str(const std::string& seq, int& i) {
     std::string res = "";
     for (; i < seq.length(); i++) {
-        if (std::isdigit(seq[i])) {
+        if (std::isdigit(seq[i]) or seq[i] == '.') {
             res += seq[i];
         }
         else {
@@ -32,12 +20,28 @@ std::string Calculator::_get_number_from_str(std::string seq, int& i) {
 }
 
 
-std::string Calculator::_convert_to_postfix_notation(const std::string seq) {
+std::string Calculator::_replace_with_aliases(const std::string& seq) {
+    std::string res = seq;
+    for (const auto& alias : aliases) {
+        size_t pos = 0;
+        while ((pos = res.find(alias.first)) != std::string::npos) {
+            res.replace(pos, alias.first.length(), alias.second);
+        }
+    }
+    return res;
+}
+
+
+std::string Calculator::_convert_to_postfix_notation(const std::string& source_seq) {
     std::string res = "";
     std::stack<char> st;
+
+    std::string seq = _replace_with_aliases(source_seq);
+
     for (int i = 0; i < seq.size(); i++) {
         char symbol = seq[i];
-        if (std::isdigit(symbol)) {
+        if (symbol == ' ') continue;
+        else if (std::isdigit(symbol)) {
             res += _get_number_from_str(seq, i) + " ";
         }
         else if (symbol == '(') {
@@ -46,73 +50,68 @@ std::string Calculator::_convert_to_postfix_notation(const std::string seq) {
         else if (symbol == ')') {
             while (st.size() > 0 && st.top() != '(') {
                 res += st.top();
+                res += ' ';
                 st.pop();
             }
             st.pop();
         }
-        else {
+        else if (_operator_priorities.count(symbol)) {
             if (symbol == '-' && (i == 0 || (i > 1 && _operator_priorities.count(seq[i - 1])))) {
                 symbol = '~';
             }
             while (st.size() > 0 && _operator_priorities[st.top()] > _operator_priorities[symbol]) {
                 res += st.top();
+                res += ' ';
                 st.pop();
             }
             st.push(symbol);
         }
+        else {
+            throw std::invalid_argument("unknown symbol in expression");
+        }
     }
     while (st.size()) {
         res += st.top();
+        res += ' ';
         st.pop();
     }
     return res;
 }
 
 
-double Calculator::calc(const std::string expression)
+double Calculator::calc(const std::string& expression)
 {
-    const std::string postfixExpr = _convert_to_postfix_notation(expression);
+    const std::string postfix_expr = _convert_to_postfix_notation(expression);
     std::stack<double> locals;
-    int counter = 0;
-
-    for (int i = 0; i < postfixExpr.length(); i++)
+    for (int i = 0; i < postfix_expr.length(); i++)
     {
-        char c = postfixExpr[i];
+        char c = postfix_expr[i];
 				
         if (std::isdigit(c))
         {
-            std::string number = _get_number_from_str(postfixExpr, i);
+            std::string number = _get_number_from_str(postfix_expr, i);
             locals.push(std::stod(number));
         }
         else if (_operator_priorities.count(c))
         {
-            counter += 1;
+            double last = locals.top();
+            locals.pop();
             if (c == '~')
             {
-                double last;
-                if (locals.size() > 0) {
-                    last = locals.top();
-                    locals.pop();
-                }
-                else {
-                    last = 0;
-                }
                 locals.push(_execute('-', 0, last));
+                // std::cout << last << " - -> " << _execute('-', 0, last) << std::endl;
+                continue;
+            }
+            if (c == 's' or c == 'c' or c == 't' or c == 'g' or c == 'e') {
+                locals.push(_execute(c, last));
+                // std::cout << last << " " << c << " -> " << _execute(c, last) << std::endl;
                 continue;
             }
 
-            double first, second;
-            if (locals.size() > 0) {
-                second = locals.top();
-                locals.pop();
-                first = locals.top();
-                locals.pop();
-            }			
-            else {
-                second = 0;
-                first = 0;
-            }
-            locals.push(_execute(c, first, second));
+            double first = locals.top();
+            locals.pop();
+            locals.push(_execute(c, first, last));		
+            // std::cout << first << " " << c << " " << last << " -> " << _execute(c,first, last) << std::endl;
         }
     }
 		
